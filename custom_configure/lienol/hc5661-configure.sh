4922e909626954ed69fe334ab0592cdc903a7d6e
Mardeson Wang <mardwangchn@outlook.com>
2020-03-29 12:36:34 +0800

    Update hc5661-configure.sh

--- custom_configure/lienol/hc5661-configure.sh
+++ custom_configure/lienol/hc5661-configure.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Modify default IP
-sed -i 's/192.168.1.1/192.168.55.11/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.55.1/g' package/base-files/files/bin/config_generate
 # Modify SSH Key
 sed -i 's/root::0:0/root:$1$fWgySbK2$CG12khZ5cp4fuwrwiC5Tn1:18336:0/g' package/base-files/files/etc/shadow
 # Modify host name
